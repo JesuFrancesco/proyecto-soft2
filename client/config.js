a8757b569414b540aaf13facdaf672a1c3b168d8
JesuFrancesco <117495071+JesuFrancesco@users.noreply.github.com>
2024-09-28 19:11:29 -0500

    feat: template from tailnext (https://github.com/onwidget/tailnext)

---
+++ client/config.js
@@ -0,0 +1,11 @@
+export const SITE = {
+    name: 'Eduyacha',
+  
+    origin: 'https://undefined.vercel.app',
+    basePathname: '/',
+    trailingSlash: false,
+  
+    title: 'Eduyacha — La educación a tus manos',
+    description: 'Eduyacha: La educación a tus manos',
+  };
+  
--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,26 @@
+import { SITE } from "./config.js"
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+  
+    trailingSlash: SITE.trailingSlash,
+    basePath: SITE.basePathname !== '/' ? SITE.basePathname : '',
+  
+    swcMinify: true,
+    poweredByHeader: false,
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'images.unsplash.com',
+        },
+        {
+          protocol: 'https',
+          hostname: 'source.unsplash.com',
+        },
+      ],
+    }
+  };
 
 export default nextConfig;
