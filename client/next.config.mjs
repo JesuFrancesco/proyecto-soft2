b3aa318287a4b3e74545b153fcbe012b506c8908
JesuFrancesco <117495071+JesuFrancesco@users.noreply.github.com>
2024-10-09 17:38:35 -0500

    .

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,31 +1,30 @@
-import { SITE } from "./config.js"
+import { SITE } from "./config.js";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: SITE.basePathname !== '/' ? SITE.basePathname : '',
-    trailingSlash: SITE.trailingSlash,
-    
-    output: "export",
-    trailingSlash: true,
-    reactStrictMode: true,
-  
-  
-    swcMinify: true,
-    poweredByHeader: false,
+  basePath: SITE.basePathname !== "/" ? SITE.basePathname : "",
+  trailingSlash: SITE.trailingSlash,
 
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'images.unsplash.com',
-        },
-        {
-          protocol: 'https',
-          hostname: 'source.unsplash.com',
-        },
-      ],
-      unoptimized: true
-    }
-  };
+  output: "export",
+  trailingSlash: true,
+  reactStrictMode: true,
+
+  swcMinify: true,
+  poweredByHeader: false,
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "source.unsplash.com",
+      },
+    ],
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
