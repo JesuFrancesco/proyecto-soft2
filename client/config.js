18170460d62fb3b977bc4cbbef422fa7fdc0a3c6
JesuFrancesco <117495071+JesuFrancesco@users.noreply.github.com>
2024-10-11 22:05:10 -0500

    ops: va mos

--- client/config.js
+++ client/config.js
@@ -1,8 +1,6 @@
 export const SITE = {
     name: 'eduyacha',
   
-    // origin: 'https://undefined.vercel.app',
-    // basePathname: '/proyecto-soft2',
     trailingSlash: false,
   
     title: 'Eduyacha — La educación a tus manos',
--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,31 +1,7 @@
-// import { SITE } from "./config.js";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   trailingSlash: true,
   reactStrictMode: true,
-  
-  // basePath: SITE.basePathname !== "/" ? SITE.basePathname : "",
-  // trailingSlash: SITE.trailingSlash,
-  // output: "export",
-
-
-  // swcMinify: true,
-  // poweredByHeader: false,
-
-  // images: {
-  //   remotePatterns: [
-  //     {
-  //       protocol: "https",
-  //       hostname: "images.unsplash.com",
-  //     },
-  //     {
-  //       protocol: "https",
-  //       hostname: "source.unsplash.com",
-  //     },
-  //   ],
-  //   unoptimized: true,
-  // },
 };
 
 export default nextConfig;
