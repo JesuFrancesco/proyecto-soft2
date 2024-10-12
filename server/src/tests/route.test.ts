977eb5f16d24fda105d065b7a009baf75706b217
JesuFrancesco <117495071+JesuFrancesco@users.noreply.github.com>
2024-10-12 13:48:48 -0500

    dou

--- server/src/tests/route.test.ts
+++ server/src/tests/route.test.ts
@@ -3,11 +3,11 @@ import { describe, expect } from "@jest/globals";
 import request from "supertest";
 
 describe("GET /api/v1/accounts", () => {
-  it("Retornar todas las cuentas", async () => {
+  it("Retornar cuenta dummy", async () => {
     return request(app)
-      .get("/api/v1/accounts")
-      .expect("Content-Type", /json/)
+      .get("/api/v1/accounts/1")
       .expect(200)
+      .expect("Content-Type", /json/)
       .then((res) => {
         expect(res.statusCode).toBe(200);
       });
