c52bfdc3e874b737a2db2fc422aeb79ef7128c9c
JesuFrancesco <117495071+JesuFrancesco@users.noreply.github.com>
2024-10-14 18:18:47 -0500

    fix: supabase location

--- client/app/auth/callback/route.ts
+++ client/app/auth/callback/route.ts
@@ -3,13 +3,13 @@ import { NextResponse } from "next/server";
 import { createClient } from "@/utils/supabase/server";
 
 export async function GET(request: Request) {
+  const supabase = createClient();
   const { searchParams, origin } = new URL(request.url);
   const code = searchParams.get("code");
   //
   const next = searchParams.get("next") ?? "/";
 
   if (code) {
-    const supabase = createClient();
     const { error } = await supabase.auth.exchangeCodeForSession(code);
     if (!error) {
       const forwardedHost = request.headers.get("x-forwarded-host");
