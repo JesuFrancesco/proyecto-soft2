3e6b7fc94b89c8b1c24cf7c4494e4fca7bfa9a50
JesuFrancesco <117495071+JesuFrancesco@users.noreply.github.com>
2024-11-27 16:59:06 -0500

    chore: nombre de pruebas de conexion

--- server/src/tests/connection.test.ts
+++ server/src/tests/connection.test.ts
@@ -3,7 +3,7 @@ import { describe, expect } from "@jest/globals";
 import request from "supertest";
 
 describe("GET /api/v1/alumnos", () => {
-  it("Retornar alumno mock", async () => {
+  it("Retornar lista de alumnos desde el servidor", async () => {
     return request(app)
       .get("/api/v1/alumnos/31")
       .expect(200)
@@ -15,7 +15,7 @@ describe("GET /api/v1/alumnos", () => {
 });
 
 describe("GET /api/v1/profesores", () => {
-  it("Retornar profesor mock", async () => {
+  it("Retornar lista de profesores desde el servidor", async () => {
     return request(app)
       .get("/api/v1/profesores/1")
       .expect(200)
@@ -26,8 +26,20 @@ describe("GET /api/v1/profesores", () => {
   });
 });
 
+describe("GET /api/v1/cursos", () => {
+  it("Retornar lista de cursos desde el servidor", async () => {
+    return request(app)
+      .get("/api/v1/clases")
+      .expect(200)
+      .expect("Content-Type", /json/)
+      .then((res) => {
+        expect(res.statusCode).toBe(200);
+      });
+  });
+});
+
 describe("GET /api/v1/paises", () => {
-  it("Retornar paises", async () => {
+  it("Retornar lista de paises desde el servidor", async () => {
     return request(app)
       .get("/api/v1/paises")
       .expect(200)
